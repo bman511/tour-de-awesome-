6b9eb65f2f919077296a89fe61bfc03b8479ef01
Brandon A <7830127+bman511@users.noreply.github.com>
2019-04-04 21:50:58 -0400

    final bump style

--- tour-de-awesome/static/js/bump.js
+++ tour-de-awesome/static/js/bump.js
@@ -154,42 +154,90 @@
 //   name: "player two",
 //   //type: "line"
 // };
-var url = "/bump_data"
+var url = "/bump_data";
 d3.json(url).then(function(data){
 
   function makeTrace(rider){
 
     let trace = {
-
+      
       x:rider.performance.stages,
       y:rider.performance.rank,
       mode:'lines+markers',
       marker:{
-        color: 'rgb(128, 0, 128)',
-        size: 8
+        size: 12
       },
+      
       line:{
-        color: 'rgb(128, 0, 128)',
+        color: $c.rand(),
         width: 4,
         shape:'spline'
       },
-      text: rider.country,
-      name: rider.name,
-
+      text: rider.name,
+      name: rider.country,
+      
     };
-
+    
     return trace;
-
+    
     }
 
 
   let trace_arr = data.map(makeTrace);
 
 // Apply the group barmode to the layout
-  let layout = {
-    title: "Place Stage by Stage",
-  //barmode: "group"
-  };
+let layout = {
+  title: "Rank By Stage",
+  hovermode:"closest",
+  yaxis: {
+  
+  zeroline: false,
+  showgrid:false,
+  autotick: false,
+  ticks: 'outside',
+  tick0: 1,
+  dtick: 10,
+  ticklen: 8,
+  tickcolor: '#000',
+
+  title:{
+
+    text: "Rank",
+    font: {
+
+      family: "Courier New, monospace",
+      size: 18,
+      color: "black"
+    }
+  },
+
+  //range: [180,0]
+  autorange: "reversed"
+},
+xaxis: {
+  zeroline:false,
+  autotick: false,
+  ticks: 'outside',
+  tick0: 1,
+  dtick: 1,
+  ticklen: 10,
+  //tickwidth: 8,
+  tickcolor: '#000',
+  title:{
+
+    text:"Stage",
+    font: {
+
+      family: "Courier New, monospace",
+      size: 18,
+      color: "black"
+       }
+     },
+          
+
+  
+  }
+};
 
 // Render the plot to the div tag with id "plot"
   Plotly.newPlot("bump", trace_arr, layout);
