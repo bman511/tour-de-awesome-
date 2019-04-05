875434d19b046a5e0cb9e4ff71f90e79538e0b1c
Linda Asiabaka <adammalynn@gmail.com>
2019-04-05 13:12:27 -0400

    updated the logicmap

--- tour-de-awesome/static/js/logicmap.js
+++ tour-de-awesome/static/js/logicmap.js
@@ -1,144 +1,9 @@
 
-//   // Creating map object
-//   var myMap = L.map("map", {
-//     center: [37.7749, -7.4194],
-//     zoom: 2.3,
-//     // layers: [streetmap, ]
-//   });
-
-//   var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-//     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-//     maxZoom: 18,
-//     id: "mapbox.streets",
-//     accessToken: API_KEY
-//   }).addTo(myMap)
-
-//   // var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-//   //   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-//   //   maxZoom: 18,
-//   //   id: "mapbox.dark",
-//   //   accessToken: API_KEY
-//   // }).addTo(myMap)
-
-//   // var baseMaps = {
-//   //   "Street Map": streetmap,
-//   //   "Dark Map": darkmap
-//   // };
-
-//   // var overlayMaps = {
-//   //   "riders": data
-//   // }
-  
-//   // L.control.layers(baseMaps, overlayMaps, {
-//   //   collapsed: false
-//   // }).addTo(myMap);
-
-//   // // Adding tile layer
-//   // L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-//   //   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-//   //   maxZoom: 18,
-//   //   id: "mapbox.streets",
-//   //   accessToken: API_KEY
-//   // }).addTo(myMap);
-
-
-
-
-
-// var url = "/countries" ;
-
-
-// // Fetch the JSON data and console log it
-// d3.json(url, function (data) { 
-//   console.log(data);
-
-  
-// country_count = data.country_count;
-// console.log(country_count);
-// country_name = geoShapes.features[4].properties.name;
-
-// // country_counts = country_count[country_name];
-
-// // console.log(country_counts);
-
-// function getColor(d, e = 0){
-  
-//   return d > 25 ? '#800026' :
-//            d > 15  ? '#BD0026' :
-//            d > 10  ? '#E31A1C' :
-//            d > 5  ? '#FC4E2A' :
-//            d > 3  ? '#e2a7fe' : 
-//            d > 1  ? '#d8fc2a' : 
-//            d > 0   ? '#76fed9' : 
-//                       '#FFEDA0';
-// }
-
-// // d > 3  ? '#FD8D3C' : #e2a7fe
-// // d > 1  ? '#FEB24C' : #d8fc2a
-// // d > 0   ? '#FED976' : #76fed9
-
-// function style(feature) {
-//   return {
-//       fillColor: getColor(country_count[feature.properties.name]),
-//       weight: 2,
-//       opacity: 1,
-//       color: 'white',
-//       dashArray: '3',
-//       fillOpacity: 0.7
-//   };
-// }
-// L.geoJson(geoShapes, {style:style}).addTo(myMap);
-// // });
-// // // Grab the data with d3
-// // d3.json(url, function(data) {
-
-// // Create a new marker cluster group
-// // var markers = L.markerClusterGroup();
-// locate = [];
-
-// for (var i = 0; i < data.country.length; i++) {
-//   var lat = data.latitude[i];
-//   var lng = data.longitude[i];
-
-//   locate[lat,lng]
-//   // lat_lngs = zip(lats, lngs)
-  
-//   console.log(lng);
-//   console.log(lat);
-//   console.log(locate) 
-
-// // Check for location property
-//   if (locate) {
-
-//     // markers.addLayer(
-//       L.circleMarker(
-  
-
-//     [lat, lng],{radius:2})
-//     .bindPopup(data.rider_name[i] + "<img width=50 src = https://i.pinimg.com/originals/05/8d/07/058d0703a96cc9f6cf669bc6017aa4bf.gif alt=testing />" + " rank -- " + data.final_ranking[i] ).addTo(myMap);
-//     console.log([i])
-//     console.log(data.rider_name[i])
-//   }
- 
-// }
-// // Add our marker cluster layer to the map
-// // myMap.addLayer(markers);
-// });
-
-
-
-
-//1st part
-// markers.addLayer(L.circleMarker([lat, lng],{radius:2}).addTo(myMap).bindPopup("<h4> " + data.rider_name  + data.country + "</h4><hr><h5>" + data.final_ranking[i] + "</h5>"  ));   
-
-
 // Creating map object
 var myMap = L.map("map", {
   center: [37.7749, -7.4194],
   zoom: 1.3
 });
-
-
 
 // Adding tile layer
 L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
@@ -148,9 +13,6 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
   accessToken: API_KEY
 }).addTo(myMap);
 
-
-
-
 var url = "/countries" ;
 
 
@@ -158,46 +20,24 @@ var url = "/countries" ;
 d3.json(url, function (data) { 
 console.log(data);
 
-
+// define variables to use for choropleth from app and geojson format in shapes.js
 country_count = data.country_count;
 console.log(country_count);
 country_name = geoShapes.features[4].properties.name;
 
-// country_counts = country_count[country_name];
-
-// console.log(country_counts);
 
 function getColor(d, e = 0){
 
-return d > 25 ? 'darkred' :
-         d > 15  ? 'red' :
-         d > 10  ? 'darkorange' :
-         d > 5  ? 'orange' :
-         d > 3  ? 'gold' : 
-         d > 1  ? 'yellow' : 
-         d > 0  ? 'greenyellow' :
-         
-                    '#FFEDA0';
+return d >= 25 ? '#fe1300' :
+       d >= 15  ? '#ff2727' :
+       d >= 10  ? '#ff6827' :
+       d >= 4  ? '#ff8514' :
+       d >= 2  ? '#ffdd27' : 
+       d > 0  ? '#ffff76' :
+                '#FFEDA0';
 }
 
 
-
-//  '#800026' :
-//          d > 15  ? '#ff7514' :
-//          d > 10  ? '#ebeb00' :
-//          d > 5  ? '#ffff27' :
-//          d > 3  ? '#ffff62' : 
-//          d > 1  ? '#FD8D3C' : 
-//          d > 0   ? '#76fed9' : 
-//                     '#FFEDA0';
-// }
-//  '#510000' :
-//          d > 15  ? '#c70000' :
-//          d > 10  ? '#ff0000' :
-//          d > 5  ? '#ff4500' :
-//          d > 3  ? '#ff3d3d' : 
-//          d > 1  ? '#ff9f9f' : 
-//          d > 0  ? '#ffdada' :
 
 function style(feature) {
 return {
@@ -250,7 +90,7 @@ var legend = L.control({ position: 'bottomright'});
 
   legend.onAdd = function() {
     var div = L.DomUtil.create('div', 'info legend'),
-        colors = [0,1,3,5,10,15,25],
+        colors = [1,2,4,10,15,25],
         labels = [];
 
     // loop through our density intervals and generate a label with a colored square for each interval
@@ -270,36 +110,32 @@ legend.addTo(myMap);
 function fillColor(colors) {
 
   switch (true) {
-    case colors > 25:
-      return 'red';
+    case colors >= 25:
+      return '#fe1300';
       break;
     
-    case colors > 15:
-      return 'orangered';
+    case colors >= 15:
+      return '#ff2727';
       break;
 
-    case colors > 10:
-      return 'darkorange';
+    case colors >= 10:
+      return '#ff6827';
       break;
     
-    case colors > 5:
-      return 'orange';
+    case colors >= 4:
+      return '#ff8514';
       break;
 
-    case colors > 3:
-      return 'gold';
-      break;
-
-    case colors > 1:
-      return 'yellow';
+    case colors >= 2:
+      return '#ffdd27';
       break;
     
     case colors > 0:
-      return 'greenyellow';
+      return '#ffff76 ';
       break;
 
     default:
-      return 'FFEDA0';
+      return '#FFEDA0';
   };
 };
 
